cdc5ae9cf79aca7415ba519ec88cb85febf06422
Nicholas Cellino <nacellin@buffalo.edu>
2022-02-26 20:51:36 -0500

    Implement EvalRuleFile in sdk

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -1,7 +1,51 @@
 package sdk
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
 
-func EvalRuleFile(path string, context map[string]interface{}) {
-	fmt.Printf("Hi from toga sdk\n")
+	"github.com/NickCellino/toga/parse"
+)
+
+func EvalRuleFile(path string, context map[string]interface{}, value *interface{}) error {
+	ruleFileContents, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("error opening rule file: %w", err)
+	}
+	ruleStr := string(ruleFileContents)
+
+	var rule interface{}
+	err = json.Unmarshal([]byte(ruleStr), &rule)
+
+	if err != nil {
+		return fmt.Errorf("invalid rule json: %v. error: %v", ruleStr, err)
+	}
+	ruleExpression, err := parse.ConvertToAst(rule)
+	if err != nil {
+		return fmt.Errorf("error parsing rule: %v", err)
+	}
+
+	contextExpression, err := parse.ParseContext(context)
+	if err != nil {
+		return fmt.Errorf("error parsing context: %v", err)
+	}
+	evaledRule, err := ruleExpression.Eval(contextExpression)
+	if err != nil {
+		return fmt.Errorf("rule evaluation error: %w", err)
+	}
+
+	expectedKind := reflect.TypeOf(*value).Kind()
+	if expectedKind == reflect.Bool {
+		*value, _ = evaledRule.AsBool()
+	} else if expectedKind == reflect.Float64 {
+		*value, _ = evaledRule.AsNumber()
+	} else if expectedKind == reflect.String {
+		*value, _ = evaledRule.AsString()
+	} else {
+		return fmt.Errorf("value passed in should be either *bool, *float64 or *string")
+	}
+
+	return nil
 }
